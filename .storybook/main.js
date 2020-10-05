a4c16cc900b914f2ce375e0afa73ccb53451735e
Emre Erdogan <emre.erdogan@socio.events>
2020-10-05 18:52:22 +0300

    initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+// module.exports = {
+//   "stories": [
+//     "../src/**/*.stories.mdx",
+//     "../src/**/*.stories.@(js|jsx|ts|tsx)"
+//   ],
+//   "addons": [
+//     "@storybook/addon-links",
+//     "@storybook/addon-essentials",
+//     "@storybook/preset-create-react-app"
+//   ]
+// }
+
+module.exports = {
+  stories: ["../src/components/**/*.stories.js"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+  ],
+};
